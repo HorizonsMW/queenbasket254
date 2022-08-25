57a3b8a8401d532e52c2a171a76f76eaa7332ff6
Brian Mulwa <mw711990838@gmail.com>
2022-08-25 15:26:49 +0300

    Mobile web interface menu fix

--- assets/js/myJs1.js
+++ assets/js/myJs1.js
@@ -1,2 +1,14 @@
 console.log("Custom JS 1 Connected");
 //alert('Site Under development');
+
+function trry(){
+    console.log('work');
+}
+
+function openNav() {
+    document.getElementById("navMenu_md").style.display = "block";
+}
+
+function closeNav() {
+    document.getElementById("navMenu_md").style.display = "none";
+}
