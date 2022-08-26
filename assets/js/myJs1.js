c92e035f859948c229f18fd78f82dc5e33d51122
Brian Mulwa <mw711990838@gmail.com>
2022-08-26 14:42:42 +0300

    JS return menu, bug fixes,  bigger menu

--- assets/js/myJs1.js
+++ assets/js/myJs1.js
@@ -5,10 +5,32 @@ function trry(){
     console.log('work');
 }
 
-function openNav() {
-    document.getElementById("navMenu_md").style.display = "block";
-}
-
 function closeNav() {
     document.getElementById("navMenu_md").style.display = "none";
+    console.log("Hide menu");
+}
+
+function openNav(){
+    console.log("Show menu");
+    document.getElementById("navMenu_md").style.display = "block";
+    let navMenu_items = `<div class="row">
+    <div class="col align-items-end" >
+        <a class="text-danger "style=" text-decoration:none; position: absolute; top: 0; right: 25px; font-size: 30px; margin-left: 50px; border:2px white;" onclick="closeNav()">&times;</a>
+    </div>
+  </div>
+  <div class="row mt-3 br-sm bg-white bg-opacity-50 p-3">
+        <div class="col">
+            <ul class="navbar-nav ms-auto">
+                <li class=""><a class="text-decoration-none text-body h6" href="index.html">Home</a></li>
+                <li class=""><a class="text-decoration-none text-body h6" href="services.html">Services</a></li>
+                <li class=""><a class="text-decoration-none text-body h6" href="products.html">Products</a></li>
+                <li class=""><a class="text-decoration-none text-body h6" href="contacts.html">Contacts</a></li>
+                <li class=""><a class="text-decoration-none text-body h6" href="login.html">Log in</a></li>
+                <li class=""><a class="text-decoration-none text-body h6" href="signup.html">Sign up</a></li>
+                <li class=""><a class="text-decoration-none text-body h6" id="cart" href="shopping-cart.html">Cart</a></li>
+            </ul>
+        </div>
+  </div>`;
+  document.getElementById('navMenu_md').innerHTML = navMenu_items;
+
 }
